7939eb29ff8c99d9392ae2a0f5e6cdbb5516361b
Luis Martinez <5863787+luis-martinez-herrera@users.noreply.github.com>
2026-04-21 14:26:39 +0200

    Update GreetingResourceTest to validate extended response schema

--- src/test/java/dev/norcode/GreetingResourceTest.java
+++ src/test/java/dev/norcode/GreetingResourceTest.java
@@ -5,17 +5,25 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 
 @QuarkusTest
 class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/hello2")
           .then()
              .statusCode(200)
                 .contentType("application/json")
-                .body("firstName", is("John"));
+                .body("firstName", is("John"))
+                .body("lastName", is("Doe"))
+                .body("age", is(30))
+                .body("options", hasSize(2))
+                .body("options[0].title", is("title 1"))
+                .body("options[0].description", is("description 1"))
+                .body("options[1].title", is("title 2"))
+                .body("options[1].description", is("description 2"));
     }
 
 }
